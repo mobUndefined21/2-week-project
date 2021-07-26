92430f885cb0636b87d3bf45f2a4639d8bc34a1a
willewilsonson <william.nordenstad@gmail.com>
2021-07-26 14:58:06 +0200

    fixed seed file

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,53 +1,77 @@
 const seeder = require('mongoose-seed');
 const faker = require('faker');
 require('dotenv').config();
-
-// try {
-//   mongoose.connect(process.env.DB_CONNECTION, 
-//     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-//     () => console.log(`db connected on user ${process.env.DB_USER}!`)
-//   );
-//   // const collection = client.db("iot").collection("kitty-litter-time-series");
-//   // collection.drop();
-
-// } catch(err) {
-//   console.log(err.message);
-// }
+const db = require('./index');
 
 const createSeedFile = () => {
   try {
     seeder.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
-      seeder.loadModels(['server/db/models/users']);
+      seeder.loadModels(['server/db/models/users', 'server/db/models/profiles']);
       console.log('connected...');
       
-      let usersCollection = [{ 'model': 'Users', 'documents': [] }];
+      let usersCollection = [];
 
-
-      for(let i=0; i<=20; i++){
+      for(let i=0; i<20; i++){
         const user = {
           firstName: faker.name.firstName(),
           lastName: faker.name.lastName(),
-          email: faker.internet.email(),
+          email: `${firstName}.${lastName}@strummer.com`,
           password: 'pass123',
         }
           console.log(`Adding user ${user.firstName}`);
-          usersCollection.documents.push(user);
+          usersCollection.push(user);
       }
-          
-      seeder.clearModels(['Users'], () => {
-        console.log('db dropped...');
-        seeder.populateModels(usersCollection, () => {
-          console.log(`populated db with ${usersCollection}`);
-          seeder.disconnect();
+
+      const seedProfiles = async () => {
+        let profileCollection = [];
+        const allUsers = await db.users.findUsers();
+        allUsers.forEach(user => {
+          const profile = {
+            name: `${user.firstName} ${user.lastName}`,
+            description: `hello my name is ${user.firstName} ${user.lastName}`,
+            user: user,
+            avatar: faker.image.avatar()
+          }
+          console.log(`Adding profile ${profile.name}`);
+          profileCollection.push(profile);
         });
+        return profileCollection;
+      }
+      
+      seeder.clearModels(['Users', 'profiles'], () => {
+        console.log('db dropped...');
+        seeder.populateModels([{ 'model': 'Users', 'documents': usersCollection}], async () => {
+          const profiles = await seedProfiles();
+          seeder.populateModels([{ 'model': 'profiles', 'documents': profiles}], () => {
+            seeder.disconnect();
+            console.log(`populated db with ${usersCollection.length} users`);
+          });
+        });
+        
+        console.log(`${usersCollection.length} users created!`);
       });
-
-      console.log(`${usersCollection.length} users created!`);
     });
-
-  } catch (err) {
-    console.log(err.message);
-  }
+      
+    } catch(err) {
+      console.log(err.message);
+    }
 }
+  
+  createSeedFile();
 
-createSeedFile();
+
+  // const seedProfiles = async (callback) => {
+  //   const allUsers = await db.users.findUsers();
+  //   allUsers.forEach(async (user) => {
+  //     await db.profiles.createProfile(
+  //       {
+  //         name: `${user.firstName} ${user.lastName}`,
+  //         description: `hello my name is ${user.firstName} ${user.lastName}`,
+  //         user: user,
+  //         avatar: faker.image.avatar()
+  //       }
+  //     );
+  //       console.log(`profile for ${user.firstName} created!`);
+  //   });
+  //   callback();
+  // };
